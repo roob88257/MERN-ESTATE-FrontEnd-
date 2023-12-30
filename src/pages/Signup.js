b2343654ea3bf9035ef098669665c4d9b1276e15
roob88257 <traj88257@gmail.com>
2023-12-30 19:45:58 +0530

    Signup feature completed successfully

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,23 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { ImSpinner8 } from "react-icons/im";
+import toast, {Toaster} from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+
 
 function Signup() {
-  return (
 
+   const [formInput,setFormInput]=useState({}); 
+   const [isLoading,setIsLoading]=useState(false);
+   
+
+   const navigate =useNavigate();
+
+   const formChangeHandler=(e)=>{
+
+   // console.log(e.target.name);
+   // console.log(e.target.value);
+   // const field=e.target.name;
+   // const value=e.target.value;
+
+   setFormInput(formInput => ({
+    ...formInput,  // Spread the existing properties
+    [e.target.name]: e.target.value,  // Update or add new properties
+  }));
+
+ 
+
+   }
+
+   const formSubmitHandler=(e)=>{
+
+    e.preventDefault();
+    setIsLoading(true);
+
+    // console.log(formInput);
+    axios.post('http://localhost:5000/api/auth/signup', formInput , {
+        headers: {
+          'Content-Type': 'application/json'
+          // Add other headers as needed
+        }
+      })
+        .then(response => {
+            toast.success(response.data);
+         
+        })
+        .catch(error => {
+          toast.error(error.message);
+        });
+    
+
+        
+
+  setIsLoading(false); 
+ 
+  setTimeout(()=>{
+    navigate('/sign-in');
+},1500);
+ // window.location.reload()
+   }
+
+  return (
     <div className='p-3 max-w-lg mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Sign Up</h1>
-        <form className='flex flex-col gap-4 '>
-        <input type="text" name="user" placeholder="Username" className='border p-3 rounded-lg focus:outline-slate-300'/>
-        <input type="text" name="email" placeholder="Email" className='border p-3 rounded-lg focus:outline-slate-300'/>
-        <input type="text" name="password" placeholder="Password" className='border p-3 rounded-lg focus:outline-slate-300'/>
-        <button type="submit" className='bg-[#334155] p-3 rounded-md text-white hover:bg-slate-600'>SIGN UP</button>
-        </form>
+        <form onSubmit={formSubmitHandler} className='flex flex-col gap-4 '>
+        <input onChange={formChangeHandler} type="text" name="username" placeholder="Username" className='border p-3 rounded-lg focus:outline-slate-300'/>
+        <input onChange={formChangeHandler} type="text" name="email" placeholder="Email" className='border p-3 rounded-lg focus:outline-slate-300'/>
+        <input onChange={formChangeHandler} type="text" name="password" placeholder="Password" className='border p-3 rounded-lg focus:outline-slate-300'/>
+        <button type="submit" disabled={isLoading} className='bg-[#334155] p-3 rounded-md text-white hover:bg-slate-600 disabled:opacity-80'>{isLoading ? 'isLoading...' : 'SIGN UP' }</button>
+        <Toaster/>    
+        </form> 
         <div className='flex flex-row gap-2 mt-4'>
             <p>
                Have an account?
             </p>
-            <Link to='\sign-in'><div className='text-blue-600 hover:cursor-pointer hover:text-blue-400'>Sign in</div></Link>
-        </div>
+            <Link to='\sign-in'><div  className='text-blue-600 hover:cursor-pointer hover:text-blue-400'>Sign in</div></Link>
+        </div>  
+       
     </div>
     
   
